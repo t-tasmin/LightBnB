eb2dc4b7b03a1a5657ed4c55d8442157de9bc8ec
Tania Tasmin <taniat.kaau@email.com>
2021-11-24 20:06:40 -0500

    Add 5 Queries

---
+++ 1_queries/5_all_my_reservations.sql
@@ -0,0 +1,8 @@
+SELECT  properties.id, properties.title, properties.cost_per_night, reservations.start_date, AVG(property_reviews.rating)
+FROM reservations 
+JOIN properties ON properties.id=reservations.property_id
+JOIN property_reviews ON properties.id= property_reviews.property_id
+WHERE reservations.guest_id=5 AND reservations.end_date < now()::date 
+GROUP BY properties.id, reservations.id
+ORDER BY reservations.start_date
+LIMIT 10;
